8eb5d490dfcb8e5dcca4dc63024663a90338b19d
tdaisuke96 <daisuke.togami@prum.jp>
2023-06-11 10:28:04 +0000

    change routes, user_profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,15 +1,23 @@
 class UserProfileController < ApplicationController
   def index
     @self_introduction = User.find(1)
+    @user = User.find(1)
     puts('self_introduction-------')
     puts(@self_introduction.to_json)
     puts('-----------------')
   end
   def update
+    #@self_introduction = User.find(1)
     puts('params check------------')
     puts(params[:self_introduction])
+    @user.update(user_params)
     #param[:self_introduction]でデータを受け取ることはできたが更新されていない
     #さらに、更新処理も要検討
 #    @self_introduction.Update(params[:self_introduction])
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:self_introduction)
+    end
 end
