22bed89f567f602430cc01f2210296a9dd573b83
tdaisuke96 <daisuke.togami@prum.jp>
2023-07-16 13:15:14 +0000

    wip:skill_edit view & controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,3 +60,33 @@ SkillDetail.create!(
   created_at: '2022-03-11 01:53:22',
   updated_at: '2022-03-11 01:53:22'
 )
+
+SkillDetail.create!(
+  id: 4,
+  skill_name: 'Java',
+  skill_level: 100,
+  skill_category_id: 1,
+  user_id: 1,
+  created_at: '2022-03-11 01:53:22',
+  updated_at: '2022-03-11 01:53:22'
+)
+
+SkillDetail.create!(
+  id: 5,
+  skill_name: 'Next.js',
+  skill_level: 50,
+  skill_category_id: 2,
+  user_id: 1,
+  created_at: '2022-03-11 01:53:22',
+  updated_at: '2022-03-11 01:53:22'
+)
+
+SkillDetail.create!(
+  id: 6,
+  skill_name: 'Azure',
+  skill_level: 0,
+  skill_category_id: 3,
+  user_id: 1,
+  created_at: '2022-03-11 01:53:22',
+  updated_at: '2022-03-11 01:53:22'
+)
