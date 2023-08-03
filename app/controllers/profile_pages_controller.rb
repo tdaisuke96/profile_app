e53f5f9f662a97671fa55ec8bb8a647f861e96bf
tdaisuke96 <daisuke.togami@prum.jp>
2023-08-03 11:48:20 +0000

    wip:skill-chart, add:chartKick

--- app/controllers/profile_pages_controller.rb
+++ app/controllers/profile_pages_controller.rb
@@ -29,7 +29,47 @@ class ProfilePagesController < ApplicationController
     puts(@user_skills.to_json)
     puts('-----------------')
     # send_image
+
+    # 3ヶ月前から現在までのデータを取得
+    start_date = 2.months.ago
+    end_date = Time.now
+
+    # バックエンド、フロントエンド、インフラの3種類のスキルカテゴリーIDを取得
+    backend_category_id = SkillCategory.find_by(skill_category: 'バックエンド').id
+    frontend_category_id = SkillCategory.find_by(skill_category: 'フロントエンド').id
+    infra_category_id = SkillCategory.find_by(skill_category: 'インフラ').id
+
+    # スキルカテゴリーごとの各月の平均スキルレベルを計算
+    backend_data = calculate_average_skill_level(backend_category_id, start_date, end_date)
+    frontend_data = calculate_average_skill_level(frontend_category_id, start_date, end_date)
+    infra_data = calculate_average_skill_level(infra_category_id, start_date, end_date)
+
+    # ビューに渡すデータをまとめる
+    @data = [
+      { name: 'バックエンド', data: backend_data },
+      { name: 'フロントエンド', data: frontend_data },
+      { name: 'インフラ', data: infra_data }
+    ]
   end
+
+  # private
+
+  # スキルカテゴリーごとの各月の平均スキルレベルを計算するメソッド
+  def calculate_average_skill_level(category_id, start_date, end_date)
+    data = {}
+    (start_date.to_date..end_date.to_date).each do |date|
+      # 各月の平均スキルレベルを計算
+      average_level = SkillDetail.where(skill_category_id: category_id, created_at: date.all_month).average(:skill_level) || 0
+      # 平均スキルレベルをデータに追加
+      data[date.strftime('%B')] = average_level
+    end
+    data
+  end
+
+
+
+
+
   
   # 画像表示用のメソッド
   def send_image
